1def034defc4e9ccff87514ea5c0c97ee50bcfd7
BABAYAGA963 <ptnssn0220@gmail.com>
2026-05-15 16:15:30 +0700

    replace next.config.ts with friend's code

--- blayblay-playlab/next.config.ts
+++ blayblay-playlab/next.config.ts
@@ -3,16 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   basePath: "/playlab",
   /* config options here */
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/playlab",
-        permanent: true,
-        basePath: false,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
